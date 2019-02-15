edc32933c4524c2353c319102b8dad64aac8f1fe
fmussoni2 <francesco.mussoni2@gmail.com>
2019-02-15 15:48:23 +0100

    inp provider local

---
+++ core/components/hybridauth/providers/Inp.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Hybridauth\Provider;
+
+use Hybridauth\Adapter\OAuth2;
+use Hybridauth\Exception\Exception;
+use Hybridauth\Data\Collection;
+use Hybridauth\User\Profile;
+use Hybridauth\Data;
+
+class Inp extends OAuth2
+{
+    protected $apiBaseUrl = 'http://api.inp.local:8080/';
+    protected $authorizeUrl = 'http://inp.local:8080/auth.html';
+    protected $accessTokenUrl = 'http://api.inp.local:8080/tokens.json';
+
+    /**
+     * @return bool|Profile
+     * @throws Exception
+     * @throws \Hybridauth\Exception\HttpClientFailureException
+     * @throws \Hybridauth\Exception\HttpRequestFailedException
+     * @throws \Hybridauth\Exception\InvalidAccessTokenException
+     */
+    function getUserProfile()
+    {
+        $accessToken = $this->getAccessToken();
+        $response = $this->httpClient->request($this->apiBaseUrl."userinfo.json","POST",array(
+            'access_token'=>$accessToken['access_token']
+        ),
+            array('Content-Type'=>'application/x-www-form-urlencoded')
+        );
+        $this->validateApiResponse('Signed API request has returned an error');
+
+        $response = (new Data\Parser())->parse($response);
+
+        $data = new Collection($response);
+
+
+        $userProfile = new Profile();
+        $userProfile->identifier = $data->get('email');
+        $userProfile->email = $data->get('email');
+        $userProfile->displayName = $data->get('fullname');
+
+        return $userProfile;
+    }
+
+}
+
